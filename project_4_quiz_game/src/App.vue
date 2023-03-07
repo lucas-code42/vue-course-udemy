4b96b010fca5bdf5328b7377bc70c0598e2ae347
lucas-code42 <lucasdearaujo.brandao@gmail.com>
2023-03-06 21:46:13 -0300

    update

--- project_4_quiz_game/src/App.vue
+++ project_4_quiz_game/src/App.vue
@@ -1,14 +1,23 @@
 <template>
     <div>
-        <h1>Oi</h1>
+        <h1>Que ano nasceu Lucas Brandao?</h1>
+
+        <input type="radio" name="options" value="True" />
+
+        <label>True</label><br />
+
+        <input type="radio" name="options" value="False" />
+
+        <label>False</label><br />
+
+        <button class="send" type="button">Send</button>
     </div>
 </template>
 
 <script>
-
 export default {
-    name: 'App',
-}
+    name: "App",
+};
 </script>
 
 <style lang="scss">
@@ -18,6 +27,22 @@ export default {
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
-    margin-top: 60px;
+    margin: 60px auto;
+    max-width: 960px;
+
+    input[type="radio"] {
+        margin: 12px 4px;
+    }
+
+    button.send {
+        margin-top: 12px;
+        height: 40px;
+        max-width: 120px;
+        padding: 0 16px;
+        color: #ffff;
+        background: #1867c0;
+        border: 1px solid #1867c0;
+        cursor: pointer;
+    }
 }
 </style>
